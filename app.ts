29b4d1caf61221f55da1de011a3e1c5a29bc89ae
zhang00001 <774004514@qq.com>
2016-11-19 10:44:10 +0800

    ,kk

---
+++ app.ts
@@ -0,0 +1,17 @@
+import * as Express from 'express';
+Express.static()
+
+
+
+
+
+// console.log('hello,i\'m app.js');
+// var express = require('express');
+// var app = express();
+// app.use(express.static(__dirname + '/public'));
+
+
+
+// app.listen(4000, () => {
+//     console.log('server is runner on 4000');
+// });
